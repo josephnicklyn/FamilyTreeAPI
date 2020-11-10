4b07b7ccd97d0253cea6780a4985b31eaa5f7c09
John Smith <jfnicklynjr@gmail.com>
2020-11-09 21:23:37 -0500

    initial commit 11/09/2020

---
+++ src/main/java/com/yakibuisness/familytree/FamilyTreeAPI/FamilyTreeApiApplication.java
@@ -0,0 +1,13 @@
+package com.yakibuisness.familytree.FamilyTreeAPI;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FamilyTreeApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FamilyTreeApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/yakibuisness/familytree/FamilyTreeAPI/FamilyTreeApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yakibuisness.familytree.FamilyTreeAPI;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FamilyTreeApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
